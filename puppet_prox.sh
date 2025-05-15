4224d4267c8bce8d194d8aff99672ff411d05fa3
RafaelMorales <nanaya2926@gmail.com>
2025-05-15 16:21:35 -0700

    Update puppet_prox.sh

--- puppet_prox.sh
+++ puppet_prox.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2025 tteck
-# Author: tteck (tteckster)
-# License: MIT
-# https://github.com/tteck/Proxmox/raw/main/LICENSE
 
 function header_info {
 clear
@@ -14,16 +10,14 @@ cat <<"EOF"
 |  __/| | | (_) | |_| | ||  __/\__ \ 
 |_|   |_|  \___/ \__,_|\__\___||___/ 
                                     
-Puppet Open Source Server & Agent Installer
+Puppet Server Installer for Proxmox LXC
 EOF
 }
 header_info
-echo -e "Loading..."
-
 APP="Puppet"
-var_disk="4"
+var_disk="6"
 var_cpu="2"
-var_ram="1024"
+var_ram="2048"
 var_os="debian"
 var_version="12"
 variables
@@ -32,57 +26,68 @@ catch_errors
 
 function default_settings() {
   CT_TYPE="1"
-  PW=""
+  PW="P@ssw0rd!"
   CT_ID=$NEXTID
-  HN=$NSAPP
+  HN="puppet-server"
   DISK_SIZE="$var_disk"
   CORE_COUNT="$var_cpu"
   RAM_SIZE="$var_ram"
   BRG="vmbr0"
   NET="dhcp"
   GATE=""
-  APT_CACHER=""
-  APT_CACHER_IP=""
-  DISABLEIP6="no"
-  MTU=""
-  SD=""
-  NS=""
-  MAC=""
-  VLAN=""
-  SSH="no"
+  SSH="yes"
   VERB="no"
   echo_default
 }
 
-function install_puppet() {
-  msg_info "Updating apt and installing prerequisites"
-  apt-get update
-  apt-get install -y wget gnupg2 curl lsb-release
+function post_install() {
+  LXC_CMD="pct exec $CTID --"
 
-  msg_info "Adding Puppet repository"
-  wget https://apt.puppet.com/puppet7-release-$(lsb_release -cs).deb -O puppet-release.deb
-  dpkg -i puppet-release.deb
-  apt-get update
+  msg_info "Actualizando contenedor"
+  $LXC_CMD apt-get update
+  $LXC_CMD apt-get upgrade -y
 
-  msg_info "Installing Puppet Server and Agent"
-  apt-get install -y puppetserver puppet-agent
+  msg_info "Instalando dependencias básicas y SSH"
+  $LXC_CMD apt-get install -y sudo wget curl gnupg2 lsb-release openssh-server
 
-  msg_info "Configuring Puppet Server to start at boot"
-  systemctl enable puppetserver
-  systemctl start puppetserver
+  msg_info "Creando usuario SSH: puppetadmin"
+  $LXC_CMD useradd -m -s /bin/bash puppetadmin
+  echo "puppetadmin:P@ssw0rd!" | $LXC_CMD chpasswd
+  $LXC_CMD usermod -aG sudo puppetadmin
 
-  msg_info "Configuring Puppet Agent to start at boot"
-  systemctl enable puppet
-  systemctl start puppet
+  msg_info "Habilitando SSH"
+  $LXC_CMD systemctl enable ssh
+  $LXC_CMD systemctl restart ssh
 
-  msg_ok "Puppet Server and Agent installation completed"
+  msg_info "Agregando repositorio oficial de Puppet"
+  $LXC_CMD wget https://apt.puppet.com/puppet7-release-bookworm.deb -O /tmp/puppet.deb
+  $LXC_CMD dpkg -i /tmp/puppet.deb
+  $LXC_CMD apt-get update
+
+  msg_info "Instalando Puppet Server y Agent"
+  $LXC_CMD apt-get install -y puppetserver puppet-agent
+
+  msg_info "Habilitando servicios de Puppet"
+  $LXC_CMD systemctl enable puppetserver
+  $LXC_CMD systemctl start puppetserver
+  $LXC_CMD systemctl enable puppet
+  $LXC_CMD systemctl start puppet
+
+  msg_ok "Puppet Server instalado correctamente"
 }
 
 start
 build_container
-install_puppet
+post_install
 
-msg_ok "Completed Successfully!\n"
-echo -e "${APP} is installed and running.\nAccess the Puppet server at port 8140.\n"
-echo -e "Default Puppet master configuration is in /etc/puppetlabs/puppet/puppet.conf\n"
-echo -e "To sign agent certificates, use: puppetserver ca sign --all\n"
+msg_ok "Instalación completada correctamente 🎉"
+
+IP=$(pct exec $CTID -- hostname -I | awk '{print $1}')
+echo -e "\nAcceso por SSH:"
+echo -e "  Usuario: puppetadmin"
+echo -e "  Contraseña: P@ssw0rd!"
+echo -e "  IP: $IP"
+echo -e "  Puerto SSH: 22"
+echo -e "  Puppet Server está escuchando en el puerto 8140\n"
+echo -e "Para firmar certificados de agentes:"
+echo -e "  sudo /opt/puppetlabs/bin/puppetserver ca sign --all\n"
